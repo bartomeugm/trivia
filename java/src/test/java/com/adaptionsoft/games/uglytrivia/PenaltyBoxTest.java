14e29dfafddc6577bd2668d69a85f1a91213ba55
Bartomeu <bartomeugm@gmail.com>
2018-11-22 15:38:38 +0100

    IsGettingOutOfPenaltyBox tested

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/PenaltyBoxTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/PenaltyBoxTest.java
@@ -1,11 +1,10 @@
 package com.adaptionsoft.games.uglytrivia;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PenaltyBoxTest {
     @ParameterizedTest
@@ -24,5 +23,15 @@ public class PenaltyBoxTest {
         penaltyBox.sendToPenaltyBox(currentPlayer);
 
         assertTrue(penaltyBox.isInPenaltyBox(currentPlayer));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, true", "2, false",
+            "3,true", "4, false",
+            "5, true", "6, false"})
+    void gets_out_of_penalty_box_if_odd_number_is_rolled(int roll, boolean expected) {
+        PenaltyBox penaltyBox = new PenaltyBox(new boolean[6]);
+        boolean actual = penaltyBox.isGettingOutOfPenaltyBox(roll);
+        assertEquals(expected, actual);
     }
 }
