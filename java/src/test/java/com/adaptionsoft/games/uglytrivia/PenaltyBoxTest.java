bdedcaeac61c37c405311e26a4bb3301f6404482
Bartomeu <bartomeugm@gmail.com>
2018-11-22 15:31:48 +0100

    isInPenaltyBox and sendToPenaltyBox tested

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/PenaltyBoxTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/PenaltyBoxTest.java
@@ -1,18 +1,28 @@
 package com.adaptionsoft.games.uglytrivia;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Collections;
-import java.util.LinkedList;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PenaltyBoxTest {
-    @Test
-    @Disabled
-    void player_should_not_be_in_penalty_box() {
-        QuestionDeck questionDeck = new QuestionDeck(Collections.emptyList());
-        Game game = new Game(questionDeck);
-        //PenaltyBox penaltyBox = new
-      //  assertFalse(penaltyBox.isInPenaltyBox(0, game));
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5})
+    void player_should_not_be_in_penalty_box(int currentPlayer) {
+        PenaltyBox penaltyBox = new PenaltyBox(new boolean[6]);
+
+        assertFalse(penaltyBox.isInPenaltyBox(currentPlayer));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5})
+    void player_should_be_sent_to_penalty_box(int currentPlayer) {
+        PenaltyBox penaltyBox = new PenaltyBox(new boolean[6]);
+
+        penaltyBox.sendToPenaltyBox(currentPlayer);
+
+        assertTrue(penaltyBox.isInPenaltyBox(currentPlayer));
     }
 }
